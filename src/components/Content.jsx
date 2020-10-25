d8581274a25c6114a63cebbd24b9d23ca56680cb
Alexander-Sergeevich-Glukhov <alexglukhov37@gmail.com>
2020-10-25 11:07:45 +0300

    learn work component

---
+++ src/components/Content.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Content = () => {
+    return (
+      <div className='Content'>
+        <ul>
+          <li>Люба</li>
+          <li>Тортик</li>
+          <li>Саша</li>
+          <li>Булочка</li>
+        </ul>
+      </div>
+    )
+  }
+
+export default Content ;  
---
+++ src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+
+const Header = () => {
+    return (
+      <div className='Header'>
+        <p>
+          Привет я хедер!!!
+        </p>
+      </div>
+    )
+}
+
+export default Header ;

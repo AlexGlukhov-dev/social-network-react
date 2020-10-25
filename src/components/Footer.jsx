3a7c01f5332f637c8960f1340809da69b021cc39
Alexander-Sergeevich-Glukhov <alexglukhov37@gmail.com>
2020-10-25 20:15:01 +0300

    added new file

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+const Footer = () => {
+    return (
+      <footer className='footer'>
+        <p>AlexGlukhov 2020 react-kabzda-kak-kruto ©</p> 
+     </footer>
+    ) ;
+  }
+
+export default Footer ;  
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+
+const NavBar = () => {
+    return (
+      <nav className='nav_bar'>
+          <ul>
+            <li><a href='#'>Profile</a></li>
+            <li><a href='#'>News</a></li>
+            <li><a href='#'>Frends</a></li>
+            <li><a href='#'>Message</a></li>
+            <li><a href='#'>Audio</a></li>
+            <li><a href='#'>Settings</a></li>
+            
+          </ul>
+      </nav>
+    );
+  }
+
+export default NavBar ;  

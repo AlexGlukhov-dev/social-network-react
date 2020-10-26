954e074b8ee7c02a9db9b111ad804b8fbb46cf98
Alexander-Sergeevich-Glukhov <alexglukhov37@gmail.com>
2020-10-26 13:58:39 +0300

    some design adjustments

--- src/components/Content/Content.jsx
+++ src/components/Content/Content.jsx
@@ -12,19 +12,22 @@ const Content = () => {
           <div className={classes.profile} >
             <p>Alex Glukhov</p>
             <ul> 
-              <li>Date of bridg : 06 may 1993</li>
+              <li>Date of birth : 06 may 1993</li>
               <li>City : Ivanovo</li>
-              <li>Education : ИГЭУ им. В.И.Ленина (2016-2021)</li>
+              <li>Education : <span>«Ivanovo State Power Engineering University»</span></li>
               <li>web-site : <a href='https://github.com/Alexander-Sergeevich-Glukhov' target='_blank'>my GitHub</a></li>
             </ul>
           </div>
         </div>
         <div className={classes.content_feed}>
             <div className={classes.new__post}>
-              <form method='get'>
-              <input type='text' value='new post' />
-              <button>Submit</button>
-              </form>
+              <div className={classes.form}>
+                <form method='get'>
+                  <textarea  cols='40' placeholder='write what you have new' className={classes.new_feed}></textarea>
+                  <button>Submit</button>
+                </form>
+              </div>
+              
               <div className={classes.post}>New Post</div>
               <div className={classes.old_posts}>
                 <p>Old Post</p>
--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -7,7 +7,7 @@ const NavBar = () => {
           <ul>
             <li><a href='#'>Profile</a></li>
             <li><a href='#'>News</a></li>
-            <li><a href='#'>Frends</a></li>
+            <li><a href='#'>Friends</a></li>
             <li><a href='#'>Message</a></li>
             <li><a href='#'>Audio</a></li>
             <li><a href='#'>Settings</a></li>

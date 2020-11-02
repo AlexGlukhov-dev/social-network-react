566ecd36f242a8cf0d8cd40c4bd7107e240eb389
Alexander-Sergeevich-Glukhov <alexglukhov37@gmail.com>
2020-11-02 10:34:12 +0300

    added id in atribut for components

--- src/components/Dialogs/Interlocutors/Interlocutor/Interlocutor.jsx
+++ src/components/Dialogs/Interlocutors/Interlocutor/Interlocutor.jsx
@@ -1,10 +1,13 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import classes from './Interlocutor.module.css' ;
 
 const Interlocutor = (props) => {
     return (
       <div className={classes.interlocutor}>   
-          <a href='/message/alex'>{props.name}</a>
+          <NavLink to={`/message/${props.id}`} activeClassName={classes.selected}>
+            {props.name}
+          </NavLink>
       </div>
     );
   }
--- src/components/Dialogs/Interlocutors/Interlocutors.jsx
+++ src/components/Dialogs/Interlocutors/Interlocutors.jsx
@@ -2,15 +2,16 @@ import React from 'react';
 import classes from './Interlocutors.module.css' ;
 import Interlocutor from './Interlocutor/Interlocutor';
 
+
 const Interlocutors = (props) => {
     return (
-      <div className={classes.interlocutors} name={props.name}>   
-          <Interlocutor name='Lubov Glukhova'/>
-          <Interlocutor name='Shamil Abdulkerimov'/>
-          <Interlocutor name='Vlad Maximov'/>
-          <Interlocutor name='Georgy Shoronov'/>
-          <Interlocutor name='Alexey Kobelev'/>
-          <Interlocutor name='Andrey Pavlenko'/>
+      <div className={classes.interlocutors}>   
+          <Interlocutor name='Lubov Glukhova' id='1'/>
+          <Interlocutor name='Shamil Abdulkerimov' id='2'/>
+          <Interlocutor name='Vlad Maximov' id='3'/>
+          <Interlocutor name='Georgy Shoronov' id='4'/>
+          <Interlocutor name='Alexey Kobelev' id='5'/>
+          <Interlocutor name='Andrey Pavlenko' id='6'/>
       </div>
     );
   }

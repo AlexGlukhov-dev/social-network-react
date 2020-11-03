54751023ded88594477f75855f244a3768cfe927
Alexander-Sergeevich-Glukhov <alexglukhov37@gmail.com>
2020-11-03 15:20:22 +0300

    change path to img

--- src/components/Dialogs/Users/Users.jsx
+++ src/components/Dialogs/Users/Users.jsx
@@ -4,12 +4,12 @@ import Interlocutor from './User/User';
 
 
 let UsersData = [
-  {id : 1, name : 'Lubov Glukhova', img_id : './../lubaha.jpg'},
-  {id : 2, name : 'Shamil Abdulkerimov', img_id : './../shamil.jpg'},
-  {id : 3, name : 'Vlad Maximov', img_id : './../vlad.jpg'},
-  {id : 4, name : 'Georgy Shoronov', img_id : './../jora.jpg'},
-  {id : 5, name : 'Alexey Kobelev', img_id : './../leha.jpg'},
-  {id : 6, name : 'Andrey Pavlenko', img_id : './../andrey.jpg'}
+  {id : 1, name : 'Lubov Glukhova', img_id : './lubaha.jpg'},
+  {id : 2, name : 'Shamil Abdulkerimov', img_id : './shamil.jpg'},
+  {id : 3, name : 'Vlad Maximov', img_id : './vlad.jpg'},
+  {id : 4, name : 'Georgy Shoronov', img_id : './jora.jpg'},
+  {id : 5, name : 'Alexey Kobelev', img_id : './leha.jpg'},
+  {id : 6, name : 'Andrey Pavlenko', img_id : './andrey.jpg'}
 ];
 
 let UsersElements = UsersData.map(user => <Interlocutor key={user.id} name={user.name} id={user.id} img_id={user.img_id}/>) ;

a193395aa5df597348228d5c57fabe1f4d1c7cce
Alexander-Sergeevich-Glukhov <alexglukhov37@gmail.com>
2020-11-06 22:33:14 +0300

    testing Ref

--- src/components/Content/Posts/Textarea/Textarea.jsx
+++ src/components/Content/Posts/Textarea/Textarea.jsx
@@ -4,14 +4,21 @@ import classes from './Textarea.module.css' ;
 
 
 const Textarea = () => {
+
+  let newPostElement = React.createRef();
+  let addPost = () => {
+    let post = newPostElement.current.value;
+    alert(post);
+  }
+
     return (
       <div className={classes.form}>
           <div>
-            <textarea  cols='50' placeholder='write what you have new....' className={classes.new_feed}></textarea>
+            <textarea ref={newPostElement} cols='50' placeholder='write what you have new....' className={classes.new_feed}></textarea>
           </div>
           <div className={classes.btns}>
             <div>
-              <button className={classes.btn}>Submit</button>
+              <button onClick={addPost} className={classes.btn}>Submit</button>
             </div>
           </div>
           
--- src/components/Dialogs/Dialog/Textarea/Textarea.jsx
+++ src/components/Dialogs/Dialog/Textarea/Textarea.jsx
@@ -4,10 +4,20 @@ import classes from './Textarea.module.css' ;
 
 
 const Textarea = () => {
+
+  let newMessage = React.createRef(); 
+  let addMessage = () => {
+      let test = newMessage.current.value ;
+      alert(test) ;
+  }
     return (
       <div className={classes.textarea}>
-        <textarea  cols='10' placeholder='write what you have new...' className={classes.new_feed}></textarea>
-        <span className={classes.btns}><button className={classes.btn}>Submit</button></span>   
+        <div className={classes.texts}>
+          <textarea ref={newMessage} cols='80' rows='1' placeholder='write what you have new...' className={classes.new_feed}></textarea>
+        </div>
+        <div className={classes.btns}>
+          <button onClick={addMessage} className={classes.btn} >Submit</button>
+        </div>   
       </div>
     );
   }
